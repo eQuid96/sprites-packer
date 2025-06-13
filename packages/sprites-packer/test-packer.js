c56963ec79b8519b09af3606ece38d8de704037a
Pier Carlo Imparato <imparatopiercarlo@gmail.com>
2025-06-13 16:36:55 +0200

    chore: switching to monorepo structure with turbo

--- packages/sprites-packer/test-packer.js
+++ packages/sprites-packer/test-packer.js
@@ -2,8 +2,8 @@ const { spritesPacker } = require("./dist/index");
 const path = require("path");
 
 spritesPacker({
-    inputPath: path.resolve(process.cwd(), "media/symbols"),
-    outputPath: path.resolve(process.cwd(), "output"),
+    inputPath: path.resolve(process.cwd(), "../../media/symbols"),
+    outputPath: path.resolve(process.cwd(), "../../output"),
     maxWidth: 2048,
     maxHeight: 2048,
     padding: 4,
